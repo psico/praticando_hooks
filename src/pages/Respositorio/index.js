827d422ce7ef6d21da596df33fffb6728e616dfd
joao.rodrigues <joao.rodrigues@embratur.gov.br>
2019-11-18 16:38:14 -0300

    [ADD] Implementação busca de detalhes do repositorio.

--- src/pages/Respositorio/index.js
+++ src/pages/Respositorio/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Container} from "./styles";
+import api from "../../services/api";
 
 export default function Repositorio({match}) {
-    return(
-        <h1 style={{color:'#FFF'}}>
-            Repositorio
-            {decodeURIComponent(match.params.repositorio)}
-        </h1>
+
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        async function load() {
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+
+        }
+
+        load();
+
+    }, [match.params.repositorio]);
+
+    return (
+        <Container style={{color: '#FFF'}}>
+
+        </Container>
     )
 }
